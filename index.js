0aeb25da59e725525fbb9365f72e3160f610945e
Joao Neto <jhonneto1221@gmail.com>
2020-03-05 22:06:28 -0300

    Indentacao e comentario

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ function verifyJWT(req, res, next){
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
     jwt.verify(token, process.env.SECRET, function(err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-    
-    // se tudo estiver ok, salva no request para uso posterior
-    req.userId = decoded.id;
-    next();
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        
+        // se tudo estiver ok, salva no request para uso posterior
+        req.user = decoded.user; // isso estava no payload
+        next();
     });
 }
 
